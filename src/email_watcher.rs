7cd01b952432ae30ad8c45f1500dad5482563fd7
Julian Andrews <jandrews271@gmail.com>
2022-10-23 12:40:35 -0400

    Fix EmailWatcher to use async channels
    The tokio runtime shares threads, so a blocking process on the email
    watcher thread prevents timer threads from running.

--- src/email_watcher.rs
+++ src/email_watcher.rs
@@ -1,6 +1,7 @@
+use futures::channel::mpsc::Receiver;
+use futures::{FutureExt, SinkExt, StreamExt};
 use std::collections::{BTreeMap, BTreeSet};
 use std::path::PathBuf;
-use std::sync::mpsc::{Receiver, Sender};
 use std::sync::{Arc, Mutex};
 
 use anyhow::Result;
@@ -27,8 +28,7 @@ impl EmailFileWatcher {
     }
 
     pub async fn run(&self, lamp_controller: Arc<Mutex<LampController>>) -> Result<()> {
-        let (tx, rx) = std::sync::mpsc::channel();
-        let _watcher = self.build_watcher(tx)?;
+        let (_watcher, mut rx) = self.build_watcher()?;
 
         // Update all watched files on first run
         let mut touched_paths: BTreeSet<PathBuf> = self.modes.keys().cloned().collect();
@@ -38,15 +38,43 @@ impl EmailFileWatcher {
                     eprintln!("Failed to update {:?}: {:?}", path, error);
                 }
             }
-            touched_paths = self.wait_for_changes(&rx)?;
+            touched_paths = self.wait_for_changes(&mut rx).await?;
         }
     }
 
+    /// Get a watcher and receiver for the files we want to monitor.
+    fn build_watcher(&self) -> Result<(RecommendedWatcher, Receiver<NotifyResult>)> {
+        // If we just watch the paths, then if the file is deleted and recreated we'll no longer be
+        // watching the inodes. By watching the containing directory instead this will continue to
+        // work unless the directory itself is removed.
+        let mut watch_dirs = BTreeSet::new();
+        for path in self.modes.keys() {
+            watch_dirs.insert(path.parent().unwrap_or_else(|| std::path::Path::new("/")));
+        }
+
+        let (mut tx, rx) = futures::channel::mpsc::channel(1);
+        let mut watcher = RecommendedWatcher::new(
+            move |res| {
+                futures::executor::block_on(async {
+                    tx.send(res).await.unwrap();
+                })
+            },
+            notify::Config::default(),
+        )?;
+        for dir in watch_dirs {
+            watcher.watch(dir, notify::RecursiveMode::NonRecursive)?;
+        }
+        Ok((watcher, rx))
+    }
+
     /// Wait for changes to the watched files and then return the set of changed files.
-    fn wait_for_changes(&self, rx: &Receiver<NotifyResult>) -> Result<BTreeSet<PathBuf>> {
-        let mut results = vec![rx.recv()?];
+    async fn wait_for_changes(&self, rx: &mut Receiver<NotifyResult>) -> Result<BTreeSet<PathBuf>> {
+        let mut results = vec![rx.next().await.expect("inotify event stream ended")];
         std::thread::sleep(std::time::Duration::from_millis(10));
-        results.extend(rx.try_iter());
+        while let Some(result) = rx.next().now_or_never() {
+            results.push(result.expect("inotify event stream ended"));
+        }
+
         let mut touched_paths = BTreeSet::new();
         for result in results {
             match result {
@@ -61,22 +89,6 @@ impl EmailFileWatcher {
             }
         }
         Ok(touched_paths)
-    }
-
-    fn build_watcher(&self, tx: Sender<NotifyResult>) -> Result<RecommendedWatcher> {
-        // If we just watch the paths, then if the file is deleted and recreated we'll no longer be
-        // watching the inodes. By watching the containing directory instead this will continue to
-        // work unless the directory itself is removed.
-        let mut watch_dirs = BTreeSet::new();
-        for path in self.modes.keys() {
-            watch_dirs.insert(path.parent().unwrap_or_else(|| std::path::Path::new("/")));
-        }
-
-        let mut watcher = RecommendedWatcher::new(tx, notify::Config::default())?;
-        for dir in watch_dirs {
-            watcher.watch(dir, notify::RecursiveMode::NonRecursive)?;
-        }
-        Ok(watcher)
     }
 
     fn update_path(
